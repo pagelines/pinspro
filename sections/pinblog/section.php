1f9222054aef157b20c4c6286dd6f9a1759c2d7b
Andrew Powers <andrew@pagelines.com>
2014-04-29 01:16:53 -0500

    post formats

--- sections/pinblog/section.php
+++ sections/pinblog/section.php
@@ -34,37 +34,66 @@ class PinBlog extends PageLinesSection {
 	}
 	
 	function get_article(){
+		$format = get_post_format();
+		$linkbox = ($format == 'quote' || $format == 'link') ? true : false;
+		
+		$gallery_format = get_post_meta( get_the_ID(), '_pagelines_gallery_slider', true);
+
+		$class[ ] = ( ! empty( $gallery_format ) ) ? 'use-flex-gallery' : '';
+		
+		$classes = apply_filters( 'pagelines_get_article_post_classes', join( " ", $class) );
 		
 		?>
 		<div class="row fix">
 			<div class="span9">
 				
-				<article class="article <?php echo join(' ', get_post_class()); ?>">
+				<article id="post-<?php the_ID(); ?>" <?php post_class( $classes ); ?>>
 					<?php  if( is_single() ): ?>
 						<div class="the-nav fix">
 							<span class="previous"><?php previous_post_link('%link', '<i class="icon icon-angle-left"></i> %title') ?></span>
 							<span class="next"><?php next_post_link('%link', '%title <i class="icon icon-angle-right"></i>') ?></span>
 						</div>
 					<?php endif; ?>
-					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'aspect-thumb' ); ?></a>
+					<?php
+						$media = pagelines_media( array( 'thumb-size' => 'aspect-thumb' ) ); 
+						
+						if( ! empty( $media ) )
+							printf( '<div class="metamedia">%s</div>', $media );
+					
+					?>
+				
+					<?php if( ! $linkbox || is_single() ): ?>
 					<div class="the-text">
-						<h2 class="title"><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
-						<div class="content">
-							<?php 
-								if( is_single() )
-									echo the_content(); 
-								else 
-									echo get_the_excerpt(); 
+						<?php if( ! $linkbox  ): ?>
+							<h2 class="title"><a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
+						<?php endif; ?>
+							<div class="content">
+								<?php 
+									if( ! is_single() ) 
+										echo get_the_excerpt();
+									else{
+										the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'pagelines' ) );
+
+										wp_link_pages( array(
+											'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'pagelines' ) . '</span>',
+											'after'       => '</div>',
+											'link_before' => '<span>',
+											'link_after'  => '</span>',
+										) );
+									}
+										
 								?>
-						</div>
+								
+							</div>
 					</div>
+					<?php endif; ?>
 					<div class="the-footer fix">
 						<?php  if( ! is_single() ): ?>
 							<a href="<?php the_permalink(); ?>">Read More <i class="icon icon-angle-right"></i></a>
 						<?php else: ?>
 							<?php previous_post_link('%link', 'Next article: %title') ?>
 						<?php endif; ?>
-						
+						<?php echo do_shortcode('[post_edit before="" after=""]');?>
 						<div class="social-shares">
 							<?php echo do_shortcode('[like_button] [pinterest] [twitter_button]');?>
 						</div>
@@ -86,7 +115,7 @@ class PinBlog extends PageLinesSection {
 						
 					</div>
 					<div class="the-footer">
-						<a href="#comments">
+						<a href="<?php the_permalink(); ?>#comments">
 							<i class="icon icon-comments"></i> <?php comments_number( '0', '1', '%s' ); ?>
 						</a>
 						<?php echo do_shortcode( '[pl_karma]' ); ?>
