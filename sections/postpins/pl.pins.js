8a301bf21fc4728e770600e88b9b2f66896251db
Simon <simon@pagelines.com>
2015-03-06 21:07:32 +0000

    increase breakpoint slightly, fixes 1600px bug

--- sections/postpins/pl.pins.js
+++ sections/postpins/pl.pins.js
@@ -2,23 +2,23 @@
 
 	// --> Initialize
 	$(document).ready(function() {
-		
-		
+
+
 		$.plPins.start()
-		
+
 	})
-	
+
 	$.plPins = {
 
 		setMasonry: function(  ){
-			
+
 				var that = this
 				,	theContainers = $('.postpin-list')
 
 
 				theContainers.each( function(){
 					var theList = $(this)
-					
+
 						theList.imagesLoaded(function(){
 
 							var windowWidth = window.innerWidth
@@ -26,7 +26,7 @@
 							,	masonrySetup = { }
 							,	numCols
 
-							if( windowWidth >= 1600 ){
+							if( windowWidth >= 1620 ){
 								numCols = 5
 							} else if ( windowWidth >= 1300 ){
 								numCols = 4
@@ -37,14 +37,16 @@
 							} else {
 								numCols = 1
 							}
-
+console.log(numCols)
+console.log(windowWidth)
+console.log(galWidth)
 							masonrySetup = {
 								columnWidth: parseInt( galWidth / numCols )
 							}
-
+console.log(masonrySetup)
 
 							theList.isotope({
-								resizable: false, 
+								resizable: false,
 								itemSelector : '.isotope-item',
 								filter: '*',
 								layoutMode: 'masonry',
@@ -53,32 +55,32 @@
 								.isotope( 'reLayout' )
 								.addClass('done-loading')
 
-			
+
 
 						})
 				})
-		
+
 		}
 
 		, start: function( opts ){
-		
+
 			var that = this
 			,	theContainers = $('.postpin-list')
-			
+
 			$(window).resize( that.setMasonry )
 			that.setMasonry( )
 
 			theContainers.each( function(){
-				
+
 				var theList = $(this)
 				,	theListID = theList.parent().data('id')
 				,	colWidth = theList.data('pin-width')
 				,	gtrWidth = theList.data('gutter-width')
 				,	loadStyle = theList.data('loading')
 				,	pinsUrl = theList.data('url')
-				
+
 				if( loadStyle == 'infinite' ){
-					
+
 					theList.infinitescroll({
 						navSelector : '.iscroll',
 						nextSelector : '.iscroll a',
@@ -90,32 +92,32 @@
 						loading: {
 							finishedMsg: 'No more pages to load.'
 						}
-						
+
 					}, function( arrayOfNewElems ) {
-						
+
 						theList.imagesLoaded(function(){
-							
-						
+
+
 							theList
 								.isotope('appended', $( arrayOfNewElems ) )
-							
-							
+
+
 						})
-						
+
 					})
 
 				} else {
-					
+
 					var theLoadLink = theList.parent().find('.fetchpins a')
 
 					theLoadLink.on('click', function(e) {
-						
+
 						e.preventDefault();
-						
+
 						theLoadLink
 							.addClass('loading')
 							.html('<i class="icon icon-refresh icon-spin spin-fast"></i> &nbsp;  Loading...');
-							
+
 						$.ajax({
 							type: "GET",
 							url: theLoadLink.attr('href') + '#pinboard',
@@ -125,16 +127,16 @@
 								var newContainer = $( out ).find( sprintf('[data-id="%s"]', theListID ) )
 								, 	result = newContainer.find( '.isotope-item' )
 								,	nextlink = newContainer.find( '.fetchpins a' ).attr('href')
-								
-								
-								
+
+
+
 								theList.append( result )
 
 								theList.imagesLoaded(function(){
-									
+
 									theList
 										.isotope('appended', result)
-										
+
 								});
 
 								theLoadLink
@@ -147,17 +149,17 @@
 									theLoadLink.attr('href', nextlink);
 								else
 									theLoadLink.parent().remove();
-								
+
 							}
 						});
 					});
 				}
-				
+
 			})
 
-			
-				
-			
+
+
+
 
 		}
 
