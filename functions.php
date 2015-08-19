65c649726ce61814f0f1ccf22c14f5d25ae405e3
Simon Prosser <simon@pagelines.com>
2015-08-20 00:06:34 +0100

    [build]

--- functions.php
+++ functions.php
@@ -8,11 +8,13 @@ require_once( 'dms/functions.php' );
 /**
  * Fix uploader issues with DMS2 and WP 4.3 jQuery temporary patch
  */
-add_action( 'wp_enqueue_scripts', 'fix_jquery_for_dms_theme' );
-function fix_jquery_for_dms_theme() {
-	if( ! is_admin() && function_exists( 'pl_draft_mode' ) && pl_draft_mode() ){
-		wp_deregister_script('jquery');
-		wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.js"), false, '1.11.2');
-		wp_enqueue_script('jquery');
+if( ! function_exists( 'fix_jquery_for_dms_theme' ) ) {
+	add_action( 'wp_enqueue_scripts', 'fix_jquery_for_dms_theme' );
+	function fix_jquery_for_dms_theme() {
+		if( ! is_admin() && function_exists( 'pl_draft_mode' ) && pl_draft_mode() ){
+			wp_deregister_script('jquery');
+			wp_register_script('jquery', ("http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.js"), false, '1.11.2');
+			wp_enqueue_script('jquery');
+		}
 	}
 }
